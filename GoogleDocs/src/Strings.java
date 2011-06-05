7aa8fafa2d0bd665aa45344ab60e61a277511959
Andrey Loskutov <iloveeclipse@gmail.com>
2011-06-05 21:44:46 +0200

    reverse words

--- GoogleDocs/src/Strings.java
+++ GoogleDocs/src/Strings.java
@@ -59,6 +59,33 @@ public class Strings {
         return newChars;
     }
 
+    static char [] reverseWords(char[] s) {
+        char [] rev = new char[s.length];
+        int lastSrcPos = -1;
+        int currDestPos = 0;
+        for (int i = s.length - 1; i >=0; i--) {
+            char c = s[i];
+            if(c == ' ' || i == 0) {
+                if(lastSrcPos >= 0) {
+                    int end = c == ' '? i +1 :i;
+                    int length = lastSrcPos - end +1;
+                    System.arraycopy(s, end, rev, currDestPos, length);
+                    currDestPos += length;
+                    rev[currDestPos++] = ' ';
+                    lastSrcPos = -1;
+                }
+            } else if(lastSrcPos < 0){
+                lastSrcPos = i;
+            }
+        }
+        if(currDestPos < rev.length - 1) {
+            for (int i = currDestPos; i < rev.length; i++) {
+                rev[i] = ' ';
+            }
+        }
+        return rev;
+    }
+
     public static void main(String[] args) {
         assert findFirstNonRepeated("aaaaaa") .equals( Character.valueOf('a') ): findFirstNonRepeated("aaaaaa");
         assert findFirstNonRepeated("abcdefg") == null : findFirstNonRepeated("abcdefg");
@@ -76,5 +103,15 @@ public class Strings {
         assert "abc".equals(new String(removeChars("abc".toCharArray(), "".toCharArray()))) : removeChars("abc", "");
         assert "abc".equals(new String(removeChars("abc".toCharArray(), "d".toCharArray()))) : removeChars("abc", "d");
         assert "".equals(new String(removeChars("".toCharArray(), "d".toCharArray()))) : removeChars("", "d");
+
+        System.out.println(new String(reverseWords("".toCharArray())));
+        System.out.println(new String(reverseWords(" ".toCharArray())));
+        System.out.println(new String(reverseWords("a ".toCharArray())));
+        System.out.println(new String(reverseWords(" a ".toCharArray())));
+        System.out.println(new String(reverseWords(" a".toCharArray())));
+        System.out.println(new String(reverseWords(" ab ".toCharArray())));
+        System.out.println(new String(reverseWords(" abc def   ".toCharArray())));
+        System.out.println(new String(reverseWords("'abc def   '".toCharArray())));
+        System.out.println(new String(reverseWords(" a b c d e f g".toCharArray())));
     }
 }

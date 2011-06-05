8230b453a07cafebdbecd12b3a99315cac4368c2
Andrey Loskutov <iloveeclipse@gmail.com>
2011-06-05 11:57:09 +0200

    "inplace" remove

--- GoogleDocs/src/Strings.java
+++ GoogleDocs/src/Strings.java
@@ -38,6 +38,27 @@ public class Strings {
         return sb.toString();
     }
 
+    static char [] removeChars(char[] s, char[] toRemove) {
+        if(s == null || s.length == 0 || toRemove == null || toRemove.length == 0) {
+            return s;
+        }
+        HashSet<Character> del = new HashSet<Character>();
+        for (int i = 0; i < toRemove.length; i++) {
+            del.add(Character.valueOf(toRemove[i]));
+        }
+        int curr = 0;
+        for (int i = 0; i < s.length; i++) {
+            char charAt = s[i];
+            if(!del.contains(Character.valueOf(charAt))) {
+                s[curr] = charAt;
+                curr ++;
+            }
+        }
+        char [] newChars = new char[curr];
+        System.arraycopy(s, 0, newChars, 0, curr);
+        return newChars;
+    }
+
     public static void main(String[] args) {
         assert findFirstNonRepeated("aaaaaa") .equals( Character.valueOf('a') ): findFirstNonRepeated("aaaaaa");
         assert findFirstNonRepeated("abcdefg") == null : findFirstNonRepeated("abcdefg");
@@ -48,5 +69,12 @@ public class Strings {
         assert "abc".equals(removeChars("abc", "")) : removeChars("abc", "");
         assert "abc".equals(removeChars("abc", "d")) : removeChars("abc", "d");
         assert "".equals(removeChars("", "d")) : removeChars("", "d");
+
+
+        assert "".equals(new String(removeChars("aaaaaaaa".toCharArray(), "a".toCharArray()))) : removeChars("aaaaaaaa", "a");
+        assert "b".equals(new String(removeChars("aaabaaaa".toCharArray(), "a".toCharArray()))) : removeChars("aaabaaaa", "a");
+        assert "abc".equals(new String(removeChars("abc".toCharArray(), "".toCharArray()))) : removeChars("abc", "");
+        assert "abc".equals(new String(removeChars("abc".toCharArray(), "d".toCharArray()))) : removeChars("abc", "d");
+        assert "".equals(new String(removeChars("".toCharArray(), "d".toCharArray()))) : removeChars("", "d");
     }
 }

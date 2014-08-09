f00c74bd2895e300442ad024fd9efd4ebf61b628
Andrey Loskutov <iloveeclipse@gmail.com>
2014-08-09 09:24:03 +0200

    Don't create strings for matching, use custom charsequence

--- GitIgnore/src/de/loskutov/gitignore/WildCardMatcher.java
+++ GitIgnore/src/de/loskutov/gitignore/WildCardMatcher.java
@@ -25,6 +25,44 @@ public class WildCardMatcher extends NameMatcher {
 
 	@Override
 	public boolean matches(String segment, int startIncl, int endExcl){
-		return p.matcher(segment.subSequence(startIncl, endExcl)).matches();
+		return p.matcher(new SubString(segment, startIncl, endExcl)).matches();
+	}
+
+	final static class SubString implements CharSequence {
+		private final String parent;
+		private final int startIncl;
+		private final int length;
+
+		public SubString(String parent, int startIncl, int endExcl) {
+			this.parent = parent;
+			this.startIncl = startIncl;
+			this.length = endExcl - startIncl;
+			if (startIncl < 0 || endExcl < 0 || length < 0
+					|| startIncl + length > parent.length()) {
+				throw new IndexOutOfBoundsException(
+						"Trying to create substring on: " + parent
+						+ " with invalid indices: " + startIncl + " / " + endExcl);
+			}
+		}
+
+		@Override
+		public final int length() {
+			return length;
+		}
+
+		@Override
+		public final char charAt(int index) {
+			return parent.charAt(startIncl + index);
+		}
+
+		@Override
+		public final CharSequence subSequence(int start, int end) {
+			return new SubString(parent, startIncl + start, startIncl + end);
+		}
+
+		@Override
+		public final String toString() {
+			return parent.substring(startIncl, startIncl + length);
+		}
 	}
 }
